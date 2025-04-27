49d015d57b0d6e431f3c829474e9025a024a3454
kareem <kareemhany59@gmail.com>
2025-04-28 01:17:29 +0200

    I added binary search to srch pat by id and sort doc by id

---
+++ src/hospital/Hospital.java
@@ -0,0 +1,17 @@
+
+package hospital;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Hospital {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
